c854243ebef5f9f9dec003be2bc1c3a3a6e7b96e
mo22sen <mohsenghafarpoor93@gmail.com>
2020-05-03 18:50:22 +0430

    config view pager

--- app/src/main/java/com/tgbs/news/ui/home/HomeFragment.kt
+++ app/src/main/java/com/tgbs/news/ui/home/HomeFragment.kt
@@ -3,6 +3,7 @@ package com.tgbs.news.ui.home
 import com.tgbs.news.R
 import com.tgbs.news.base.BaseFragment
 import com.tgbs.news.databinding.FragmentHomeBinding
+import kotlinx.android.synthetic.main.fragment_home.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeFragment : BaseFragment<HomeViewModel, FragmentHomeBinding>() {
@@ -12,6 +13,9 @@ class HomeFragment : BaseFragment<HomeViewModel, FragmentHomeBinding>() {
     override val layoutRes: Int = R.layout.fragment_home
 
     override fun configEvents() {
+        val fragmentAdapter = HomePagerAdapter(childFragmentManager)
+        view_pager.adapter = fragmentAdapter
+        tabs_feed.setupWithViewPager(view_pager)
     }
 
     override fun bindObservables() {
