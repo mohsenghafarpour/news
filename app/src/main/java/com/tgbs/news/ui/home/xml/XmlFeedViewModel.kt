11f79741ad19a1120c4f7f226b9d62ec0dd0d30e
mo22sen <mohsenghafarpoor93@gmail.com>
2020-05-05 04:28:23 +0430

    get data from db

--- app/src/main/java/com/tgbs/news/ui/home/xml/XmlFeedViewModel.kt
+++ app/src/main/java/com/tgbs/news/ui/home/xml/XmlFeedViewModel.kt
@@ -1,26 +1,18 @@
 package com.tgbs.news.ui.home.xml
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.tgbs.news.base.BaseViewModel
 import com.tgbs.news.data.Detail
 import com.tgbs.news.data.repository.NewsRepository
-import com.tgbs.news.network.Result
-import com.tgbs.news.utils.ktx.logD
 import kotlinx.coroutines.launch
 
 class XmlFeedViewModel(private val newsRepository: NewsRepository) : BaseViewModel() {
 
-    private val _news = MutableLiveData<List<Detail>>()
-    val news: LiveData<List<Detail>>
-        get() = _news
+    val news: LiveData<List<Detail>> = newsRepository.getNews()
 
     private fun getNewsFromNetwork() = viewModelScope.launch {
-        when (val result = newsRepository.getNewsFromNetwork()) {
-            is Result.Success -> _news.postValue(result.data.channel?.details)
-            is Result.Error -> logD("error this part")
-        }
+        newsRepository.getNewsFromNetwork()
     }
 
     init {
