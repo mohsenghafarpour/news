c6f9f761595fba46a45ec7b8bf843b4b981f27e6
mo22sen <mohsenghafarpoor93@gmail.com>
2020-05-05 03:30:33 +0430

    added mld to get data from repository and observe to recycler view

--- app/src/main/java/com/tgbs/news/ui/home/xml/XmlFeedViewModel.kt
+++ app/src/main/java/com/tgbs/news/ui/home/xml/XmlFeedViewModel.kt
@@ -1,14 +1,26 @@
 package com.tgbs.news.ui.home.xml
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.tgbs.news.base.BaseViewModel
+import com.tgbs.news.data.Detail
 import com.tgbs.news.data.repository.NewsRepository
+import com.tgbs.news.network.Result
+import com.tgbs.news.utils.ktx.logD
 import kotlinx.coroutines.launch
 
 class XmlFeedViewModel(private val newsRepository: NewsRepository) : BaseViewModel() {
 
+    private val _news = MutableLiveData<List<Detail>>()
+    val news: LiveData<List<Detail>>
+        get() = _news
+
     private fun getNewsFromNetwork() = viewModelScope.launch {
-        newsRepository.getNewsFromNetwork()
+        when (val result = newsRepository.getNewsFromNetwork()) {
+            is Result.Success -> _news.postValue(result.data.channel?.details)
+            is Result.Error -> logD("error this part")
+        }
     }
 
     init {
