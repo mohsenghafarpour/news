67cf36d7580c0bf14ed2f7cd5d7c19fb498e2ea1
mo22sen <mohsenghafarpoor93@gmail.com>
2020-05-05 04:24:45 +0430

    upsert data and get data from db

--- app/src/main/java/com/tgbs/news/data/repository/NewsRepositoryImpl.kt
+++ app/src/main/java/com/tgbs/news/data/repository/NewsRepositoryImpl.kt
@@ -1,15 +1,24 @@
 package com.tgbs.news.data.repository
 
+import androidx.lifecycle.LiveData
+import com.tgbs.news.data.Detail
 import com.tgbs.news.data.News
+import com.tgbs.news.data.db.NewsDao
 import com.tgbs.news.network.Result
 import com.tgbs.news.network.api.NewsApi
 import com.tgbs.news.utils.safeApiCall
 
-class NewsRepositoryImpl(private val newsApi: NewsApi) : NewsRepository {
+class NewsRepositoryImpl(
+    private val newsApi: NewsApi,
+    private val newsDao: NewsDao
+) : NewsRepository {
 
     override suspend fun getNewsFromNetwork(): Result<News> = safeApiCall {
         val response = newsApi.getNews()
+        newsDao.clearNews()
+        response.channel?.details?.let { newsDao.upsertNewsDetail(it) }
         return@safeApiCall Result.Success(response)
     }
 
+    override fun getNews(): LiveData<List<Detail>> = newsDao.getNewsDetail()
 }
