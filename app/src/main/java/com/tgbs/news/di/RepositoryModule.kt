fce769c8056e8c88778f433eac5491e5fad7fae2
mo22sen <mohsenghafarpoor93@gmail.com>
2020-05-04 15:58:46 +0430

    added repository module to provide di

---
+++ app/src/main/java/com/tgbs/news/di/RepositoryModule.kt
@@ -0,0 +1,9 @@
+package com.tgbs.news.di
+
+import com.tgbs.news.data.repository.NewsRepository
+import com.tgbs.news.data.repository.NewsRepositoryImpl
+import org.koin.dsl.module
+
+val repositoryModule = module {
+    single<NewsRepository> { NewsRepositoryImpl(get()) }
+}
