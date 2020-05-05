8b513eb8786a4f3be73edea3072ee3ea7d232001
mo22sen <mohsenghafarpoor93@gmail.com>
2020-05-05 15:22:02 +0430

    upsert data in db and get from db to show list

--- app/src/main/java/com/tgbs/news/data/repository/NewsRepositoryImpl.kt
+++ app/src/main/java/com/tgbs/news/data/repository/NewsRepositoryImpl.kt
@@ -1,24 +1,39 @@
 package com.tgbs.news.data.repository
 
 import androidx.lifecycle.LiveData
-import com.tgbs.news.data.Detail
-import com.tgbs.news.data.News
 import com.tgbs.news.data.db.NewsDao
+import com.tgbs.news.data.pojo.Article
+import com.tgbs.news.data.pojo.Detail
+import com.tgbs.news.data.pojo.NewsEn
+import com.tgbs.news.data.pojo.NewsFa
 import com.tgbs.news.network.Result
-import com.tgbs.news.network.api.NewsApi
+import com.tgbs.news.network.api.NewsEnApi
+import com.tgbs.news.network.api.NewsFaApi
 import com.tgbs.news.utils.safeApiCall
 
 class NewsRepositoryImpl(
-    private val newsApi: NewsApi,
+    private val newsApi: NewsFaApi,
+    private val newsEnApi: NewsEnApi,
     private val newsDao: NewsDao
 ) : NewsRepository {
 
-    override suspend fun getNewsFromNetwork(): Result<News> = safeApiCall {
+    override suspend fun getNewsFaFromNetwork(): Result<NewsFa> = safeApiCall {
         val response = newsApi.getNews()
-        newsDao.clearNews()
-        response.channel?.details?.let { newsDao.upsertNewsDetail(it) }
+        newsDao.clearNewsFa()
+        response.channel?.details?.let { newsDao.upsertNewsFaDetail(it) }
         return@safeApiCall Result.Success(response)
     }
 
-    override fun getNews(): LiveData<List<Detail>> = newsDao.getNewsDetail()
+
+    override suspend fun getNewsEnFromNetwork(source: String, apiKey: String): Result<NewsEn> =
+        safeApiCall {
+            val response = newsEnApi.getNewsEn(source, apiKey)
+            newsDao.clearNewsEn()
+            newsDao.upsertNewsEnDetail(response.articles)
+            return@safeApiCall Result.Success(response)
+        }
+
+    override fun getNewsFa(): LiveData<List<Detail>> = newsDao.getNewsFaDetail()
+
+    override fun getNewsEn(): LiveData<List<Article>> = newsDao.getNewsEnDetail()
 }
