2fd026238998ed5c30a764f34fb2be1e7fda33a2
mo22sen <mohsenghafarpoor93@gmail.com>
2020-05-05 15:21:22 +0430

    update db

--- app/src/main/java/com/tgbs/news/data/db/NewsDB.kt
+++ app/src/main/java/com/tgbs/news/data/db/NewsDB.kt
@@ -2,12 +2,14 @@ package com.tgbs.news.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.tgbs.news.data.Detail
+import com.tgbs.news.data.pojo.Article
+import com.tgbs.news.data.pojo.Detail
 
 
 @Database(
     entities = [
-        Detail::class
+        Detail::class,
+        Article::class
     ], version = 1, exportSchema = false
 )
 
--- app/src/main/java/com/tgbs/news/data/db/NewsDao.kt
+++ app/src/main/java/com/tgbs/news/data/db/NewsDao.kt
@@ -5,18 +5,33 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.tgbs.news.data.Detail
+import com.tgbs.news.data.pojo.Article
+import com.tgbs.news.data.pojo.Detail
 
 @Dao
 interface NewsDao {
 
-    @Query("SELECT * FROM news")
-    fun getNewsDetail(): LiveData<List<Detail>>
+    /*xml news_fa*/
+
+    @Query("SELECT * FROM news_fa")
+    fun getNewsFaDetail(): LiveData<List<Detail>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun upsertNewsDetail(details: List<Detail>)
+    suspend fun upsertNewsFaDetail(details: List<Detail>)
 
-    @Query("DELETE FROM news")
-    suspend fun clearNews()
+    @Query("DELETE FROM news_fa")
+    suspend fun clearNewsFa()
+
+    /*json - news_en*/
+
+    @Query("SELECT * FROM news_en")
+    fun getNewsEnDetail(): LiveData<List<Article>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsertNewsEnDetail(details: List<Article>)
+
+    @Query("DELETE FROM news_en")
+    suspend fun clearNewsEn()
+
 
 }
