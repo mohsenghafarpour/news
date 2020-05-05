6b4edd87a3b685f820d65ea57f1cb39abca442cd
mo22sen <mohsenghafarpoor93@gmail.com>
2020-05-05 15:25:30 +0430

    rename function

--- app/src/main/java/com/tgbs/news/ui/home/xml/XmlFeedFragment.kt
+++ app/src/main/java/com/tgbs/news/ui/home/xml/XmlFeedFragment.kt
@@ -3,7 +3,7 @@ package com.tgbs.news.ui.home.xml
 import com.tgbs.news.R
 import com.tgbs.news.base.BaseFragment
 import com.tgbs.news.databinding.FragmentXmlFeedBinding
-import com.tgbs.news.ui.NewsXmlAdapter
+import com.tgbs.news.ui.NewsFaAdapter
 import kotlinx.android.synthetic.main.fragment_xml_feed.*
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -11,7 +11,7 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 class XmlFeedFragment : BaseFragment<XmlFeedViewModel, FragmentXmlFeedBinding>() {
 
     override val viewModel: XmlFeedViewModel by viewModel()
-    private val adapter: NewsXmlAdapter by inject()
+    private val adapter: NewsFaAdapter by inject()
 
     override val layoutRes: Int = R.layout.fragment_xml_feed
 
