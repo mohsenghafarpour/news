32065bccc38dd6fb5cf3232ba239bf2131dbdc9c
mo22sen <mohsenghafarpoor93@gmail.com>
2020-05-05 15:26:05 +0430

    rename function

--- app/src/main/java/com/tgbs/news/ui/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/tgbs/news/ui/favorite/FavoriteFragment.kt
@@ -3,14 +3,14 @@ package com.tgbs.news.ui.favorite
 import com.tgbs.news.R
 import com.tgbs.news.base.BaseFragment
 import com.tgbs.news.databinding.FragmentFavoriteBinding
-import com.tgbs.news.ui.NewsXmlAdapter
+import com.tgbs.news.ui.NewsFaAdapter
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class FavoriteFragment : BaseFragment<FavoriteViewModel, FragmentFavoriteBinding>() {
 
     override val viewModel: FavoriteViewModel by viewModel()
-    private val adapter: NewsXmlAdapter by inject()
+    private val adapter: NewsFaAdapter by inject()
     override val layoutRes: Int = R.layout.fragment_favorite
 
     override fun configEvents() {
