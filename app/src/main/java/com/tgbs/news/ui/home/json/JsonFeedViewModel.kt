bc1ff9b6c50b647ba894893bd744f625e0e1dfb3
mo22sen <mohsenghafarpoor93@gmail.com>
2020-05-05 15:25:06 +0430

    get data from db and showing in list

--- app/src/main/java/com/tgbs/news/ui/home/json/JsonFeedViewModel.kt
+++ app/src/main/java/com/tgbs/news/ui/home/json/JsonFeedViewModel.kt
@@ -1,6 +1,25 @@
 package com.tgbs.news.ui.home.json
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
 import com.tgbs.news.base.BaseViewModel
+import com.tgbs.news.data.pojo.Article
+import com.tgbs.news.data.repository.NewsRepository
+import kotlinx.coroutines.launch
 
-class JsonFeedViewModel : BaseViewModel() {
+private const val SOURCE = "techcrunch"
+private const val API_KEY = "95a7a91b22f74929b6159a2a4ea22cee"
+
+class JsonFeedViewModel(private val newsRepository: NewsRepository) : BaseViewModel() {
+
+    val news: LiveData<List<Article>> = newsRepository.getNewsEn()
+
+    private fun getNewsEnFromNetwork() = viewModelScope.launch {
+        newsRepository.getNewsEnFromNetwork(SOURCE, API_KEY)
+    }
+
+    init {
+        getNewsEnFromNetwork()
+    }
+
 }
