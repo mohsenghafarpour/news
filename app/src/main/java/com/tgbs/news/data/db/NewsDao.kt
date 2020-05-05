3edc2cd6a411653a54c39d1ee199bd2ccddccc17
mo22sen <mohsenghafarpoor93@gmail.com>
2020-05-06 01:38:33 +0430

    added new query for favorite

--- app/src/main/java/com/tgbs/news/data/db/NewsDao.kt
+++ app/src/main/java/com/tgbs/news/data/db/NewsDao.kt
@@ -16,22 +16,41 @@ interface NewsDao {
     @Query("SELECT * FROM news_fa")
     fun getNewsFaDetail(): LiveData<List<Detail>>
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun upsertNewsFaDetail(details: List<Detail>)
 
-    @Query("DELETE FROM news_fa")
-    suspend fun clearNewsFa()
+    @Query("UPDATE news_fa SET isFavorite=1 WHERE guid =:id")
+    suspend fun favoriteNewsFa(id: String)
+
+    @Query("UPDATE news_fa SET isFavorite=0 WHERE guid =:id")
+    suspend fun unFavoriteNewsFa(id: String)
+
+    @Query("SELECT * FROM news_fa WHERE isFavorite=1")
+    fun getFavoriteNewsFa(): LiveData<List<Detail>>
+
+    @Query("SELECT * FROM news_fa WHERE guid=:id")
+    suspend fun findNewsFaById(id: String): Detail?
+
 
     /*json - news_en*/
 
     @Query("SELECT * FROM news_en")
     fun getNewsEnDetail(): LiveData<List<Article>>
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun upsertNewsEnDetail(details: List<Article>)
 
-    @Query("DELETE FROM news_en")
-    suspend fun clearNewsEn()
+    @Query("UPDATE news_en SET isFavorite=1 WHERE url =:id")
+    suspend fun favoriteNewsEn(id: String)
+
+    @Query("UPDATE news_en SET isFavorite=0 WHERE url =:id")
+    suspend fun unFavoriteNewsEn(id: String)
+
+    @Query("SELECT * FROM news_en WHERE isFavorite=1")
+    fun getFavoriteNewsEn(): LiveData<List<Article>>
+
+    @Query("SELECT * FROM news_en WHERE url=:id")
+    suspend fun findNewsEnById(id: String): Article?
 
 
 }
