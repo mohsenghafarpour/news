439bcfc5bc8343543ee7c4219021f8f5adc92dbb
mo22sen <mohsenghafarpoor93@gmail.com>
2020-05-06 01:42:38 +0430

    create favorite page

--- app/src/main/java/com/tgbs/news/ui/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/tgbs/news/ui/favorite/FavoriteViewModel.kt
@@ -1,8 +1,30 @@
 package com.tgbs.news.ui.favorite
 
+import androidx.lifecycle.viewModelScope
 import com.tgbs.news.base.BaseViewModel
+import com.tgbs.news.data.pojo.Article
+import com.tgbs.news.data.pojo.Detail
+import com.tgbs.news.data.pojo.News
+import com.tgbs.news.data.pojo.NewsType
 import com.tgbs.news.data.repository.NewsRepository
+import kotlinx.coroutines.launch
 
 class FavoriteViewModel(private val newsRepository: NewsRepository) : BaseViewModel() {
+
+    val favoriteNews = newsRepository.getFavoriteNews()
+
+    fun goToDetailNews(news: News?) {
+        val (id, newsType) = when (news) {
+            is Article -> news.link to NewsType.ARTICLE
+            is Detail -> news.guid to NewsType.DETAIL
+            else -> null to null
+        }
+        if (id == null || newsType == null) return
+        navigateTo(FavoriteFragmentDirections.actionFavoriteToDetail(id, newsType))
+    }
+
+    fun onFavoriteClicked(news: News) = viewModelScope.launch {
+        newsRepository.toggleFavoriteNews(news)
+    }
 
 }
