7f8571e6d857d2d1713c709c464e6d4bbc57238f
mo22sen <mohsenghafarpoor93@gmail.com>
2020-05-05 13:20:27 +0430

    added list in favorite view

--- app/src/main/java/com/tgbs/news/ui/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/tgbs/news/ui/favorite/FavoriteFragment.kt
@@ -3,15 +3,21 @@ package com.tgbs.news.ui.favorite
 import com.tgbs.news.R
 import com.tgbs.news.base.BaseFragment
 import com.tgbs.news.databinding.FragmentFavoriteBinding
+import com.tgbs.news.ui.NewsXmlAdapter
+import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class FavoriteFragment : BaseFragment<FavoriteViewModel, FragmentFavoriteBinding>() {
 
     override val viewModel: FavoriteViewModel by viewModel()
-
+    private val adapter: NewsXmlAdapter by inject()
     override val layoutRes: Int = R.layout.fragment_favorite
 
     override fun configEvents() {
+//        list_news.adapter = adapter
+//        adapter.onItemClicked = { item , _ ->
+//            viewModel.goToDetailNews(item)
+//        }
     }
 
     override fun bindObservables() {
